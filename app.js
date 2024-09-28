6a8814977abe6c55ea6af102b377ac822f75dc8d
Francis Duong <francisduong@Franciss-MacBook-Pro.local>
2024-09-28 09:39:14 +0700

    and group for reviews

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("express-async-errors");
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV === "development") {
 const authRouter = require("./routes/authRoute");
 const userRouter = require("./routes/userRoute");
 const productRouter = require("./routes/productRoute");
+const reviewRouter = require("./routes/reviewRoute");
 
 // Middlewares
 const notFoundMiddleware = require("./middlewares/not-found");
@@ -21,10 +23,13 @@ const errorHanlerMiddleware = require("./middlewares/error-handler");
 
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_TOKEN_SECRET));
+app.use(express.static("./public"));
+app.use(fileUpload());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHanlerMiddleware);
---
+++ controllers/reviewController.js
@@ -0,0 +1,92 @@
+const Review = require("./../models/reviewModel");
+const Product = require("./../models/productModel");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("./../errors");
+const { checkPermissions } = require("./../utils/index");
+
+const createReview = async (req, res) => {
+  const { product: productId } = req.body;
+  const isValidProduct = await Product.findOne({ _id: productId });
+  if (!isValidProduct)
+    throw new CustomError.BadRequestError(
+      `Not Found product with this ID: ${productId}`
+    );
+
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+  if (alreadySubmitted)
+    throw new CustomError.BadRequestError(
+      "Already Submitted review for this product"
+    );
+
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+  res.status(StatusCodes.CREATED).json({ review });
+};
+
+const getAllReviews = async (req, res) => {
+  const reviews = await Review.find().populate({
+    path: "product",
+    select: "name company price",
+  });
+  res.status(StatusCodes.OK).json({ reviews, count: reviews.length });
+};
+
+const getSingleReview = async (req, res) => {
+  const { id: reviewId } = req.params;
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review)
+    throw new CustomError.BadRequestError(
+      `Not Found review with this ID: ${reviewId}`
+    );
+  res.status(StatusCodes.OK).json({ review });
+};
+
+const updateReview = async (req, res) => {
+  const { id: reviewId } = req.params;
+  const { rating, title, comment } = req.body;
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review)
+    throw new CustomError.BadRequestError(
+      `Not Found review with this ID: ${reviewId}`
+    );
+  checkPermissions(req.user, review._id);
+
+  review.rating = rating;
+  review.title = title;
+  review.comment = comment;
+
+  await review.save();
+  res.status(StatusCodes.OK).json({ review });
+};
+
+const deleteReview = async (req, res) => {
+  const { id: reviewId } = req.params;
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review)
+    throw new CustomError.BadRequestError(
+      `Not Found review with this ID: ${reviewId}`
+    );
+  checkPermissions(req.user, review._id);
+  await review.remove();
+  res
+    .status(StatusCodes.NO_CONTENT)
+    .json({ msg: "Delete review successfully !" });
+};
+
+const getSingleProductReviews = async (req, res) => {
+  const { id: productId } = req.params;
+  const reviews = await Review.find({ product: productId });
+  res.status(StatusCodes.OK).json({ reviews, count: reviews.length });
+};
+
+module.exports = {
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview,
+  getSingleProductReviews,
+};
